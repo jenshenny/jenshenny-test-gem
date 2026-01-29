095a3cc98bd26b401a7dc91f0458b2f2a17ea7c8
Jenny Shen <jenny.shen@shopify.com>
2026-01-28 23:01:22 -0500

    Initial commit

---
+++ lib/jenshenny/test/gem.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "gem/version"
+
+module Jenshenny
+  module Test
+    module Gem
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jenshenny/test/gem/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Jenshenny
+  module Test
+    module Gem
+      VERSION = "0.1.0"
+    end
+  end
+end
